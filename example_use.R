a3dc8e75d2c694f4b29fb59a03ab419b78602eb5
Robert J. Kim <robert7kim7@gmail.com>
2018-03-04 23:16:50 -0800

    i think white to red or white to black gradient looks good on map

--- example_use.R
+++ example_use.R
@@ -1,10 +1,11 @@
-# api.key <- "put your api key here"
+# Add API key here:
+api.key <- " "
 source("./processing.R")
+
 
 coord.frame <- read.csv("./coordinates.csv", stringsAsFactors = FALSE)
 weather.frame <- create.weather.frame(coord.frame)
 
 source("./map.R")
-
 
 print(conditions.report)
--- map.R
+++ map.R
@@ -5,16 +5,16 @@ library(ggrepel)
 # Get Data for Washington Map:
 wa <- get_googlemap(center = "soap lake wa", zoom = 6, size = c(400, 250), maptype = "roadmap")
 
+
 # Creates base map, to be layered on later with ski resorts.
 base.map <- ggmap(wa) + ggtitle("Ski Resort Conditions in WA:") + 
   theme(plot.title = element_text(hjust = 0.5, size = 15))
 
 # Creates map of WA, & points of ski resorts:
 conditions.report <- ggmap(wa) + 
-  geom_point(data = weather.frame, aes(x = long, y = lat, fill = rating, color = rating), alpha = .7, size = 5, shape = 21) + 
+  geom_point(data = weather.frame, aes(x = long, y = lat, fill = rating, color = rating), size = 8, shape = 21) + 
   geom_label_repel(data = weather.frame, aes(x = long, y = lat, label = name), nudge_x = .5, nudge_y = .15) +
   guides(fill=FALSE, alpha=FALSE, size=FALSE)  + ggtitle("Ski Resort Conditions in WA:") + theme(plot.title = element_text(hjust = 0.5, size = 30)) +
-  scale_fill_gradient(low = "red", high = "green", limits=c(1, 10)) +
-  scale_color_gradient(low = "red", high = "green",limits=c(1, 10))
+  scale_fill_gradient(low = "white", high = "red", limits=c(1, 10)) +
+  scale_color_gradient(low = "white", high = "red",limits=c(1, 10))
 
-print(conditions.report)
